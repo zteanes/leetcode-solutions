719ab85f6936ef42e491387f3dd6891e384b7555
zteanes <zacharyeanes2020@gmail.com>
2023-07-19 11:39:17 -0400

    fleshed out java solution to majority element

--- Majority Element/majorityElement.java
+++ Majority Element/majorityElement.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int majorityElement(int[] nums) {
+        /* Solution 1 */
         // multiple really good approaches to this, first being sorting and grabbing
         // the element in the middle. because we know the element will appear more than
         // n/2 times, we can sort the array and grab the middle element.
         Arrays.sort(nums);
         return nums[nums.length / 2];
+    }
 
-        // another is to use Moore's Voting algorithm to choose a candidate
+    public int majorityElement2(int[] nums){
+        /* Solution 2 */
+        // another is to use Moore's Voting algorithm to choose a candidate. builds on idea
+        // that the element most present will automatically be incremented most whenever
+        // it is most prevalent and will end as our choice. algorithm follows that we 
+        // increment whenever we encounter current choice, and decrement otherwise
         int cnt = 0;
         int choice = 0;
 
